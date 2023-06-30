6488f08512b9fda462d85ace35731ca6eb391a43
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2023-07-01 02:32:10 +0530

    Levii added hello.py file

---
+++ hello.py
@@ -0,0 +1,10 @@
+x = 4 
+if x>2:
+    print('Bigger')
+else:
+    print('Smaller')
+print('All Done')
+
+Yahi aata hai Python mein mujhe 
+chalo main chalta hoon bhai ab
+koi mujhe disturb nhi kkrega!

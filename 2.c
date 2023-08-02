ff88000696fcfb5b1a252fdc4d160ee83bbdf505
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-02 23:30:10 +0530

    First commit

--- 2.c
+++ 2.c
@@ -5,6 +5,19 @@ int main(){
     int i=1;
     scanf("%d", &t);
     while(i<=t){
-        
+        scanf("%d %d", &A, &B);
+        if(A/B==B/A && A==B){
+            printf("Both %d and %d are Equal!", A, B);
+        }
+        else if(A/B==B/A){
+            printf("Both %d and %d are Proportional to each other!", A, B);
+        }
+        else{
+            printf("Both %d and %d are Not Equal!", A, B);
+        }
+        i=i+1;
+
     }
+    return 0;
 }
+// Ohh my Godd!

938a9801c00677b499bb65a161ef6993ee906467
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-03 22:25:02 +0530

    First commit

--- 3.c
+++ 3.c
@@ -2,20 +2,20 @@
 int main(){
     int t;
     int X,Y,Z;
-    scanf("%d", &t);
+    scanf("%d", &t);      // FARM ANIMALS!
     while(t--){
         scanf("%d %d %d", &X, &Y, &Z);
         if(Z%X==0 && Z%Y==0){
-            printf("ANY\n");
+            printf("ANY\n");  // Either CHICKEN or DUCK
         }
         else if(Z%X==0){
-            printf("CHICKEN\n");
+            printf("CHICKEN\n"); // Only CHICKEN
         }
         else if(Z%Y==0){
-            printf("DUCK\n");
+            printf("DUCK\n");  // Only DUCK
         }
         else{
-            printf("NONE\n");
+            printf("NONE\n");  // Neither CHICKEN nor DUCK
         }
 
     }

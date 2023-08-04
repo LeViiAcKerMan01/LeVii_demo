440b0da70529ff2935b79299f08838f5f23acc64
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-04 22:35:46 +0530

    First commit

--- 1.c
+++ 1.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
-
 int main()
 {
-    int t,A,B;
-    int i=1;
-    scanf("%d", &t);
-    while(i<=t){
-        scanf("%d %d", &A, &B);
-        if(A%2==0 && B%2==0){
-            printf("%d\n", ((A+B)/2));
-        }
-        else if(A%2!=0 && B%2!=0){
-            printf("%d\n", ((A+B)/2));
-        }
-        else{
-            printf("%d\n", -1);
-        }
-        i=i+1;
-        
-    }
-    return 0;
+int A,B,C;
+int t;
+B=(A+C)/2;
+scanf("d", &t);
+while(t--){
+if(A%2==0 && C%2==0)
+{
+printf("%d\n", B);
+}
+else if(A%2!=0 && C%2!=0)
+{
+printf("%d\n", B);
+}
+else
+{
+printf("%d\n", -1);
+}
+}
+return 0;
 }
 //Great man!! What the Heck! I did a new invention Mom! ohh My Godd!

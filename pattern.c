5dfc008f03ccef29962c0e357cb063deafeca38a
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-09 22:55:33 +0530

    First commit

---
+++ pattern.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+      // Complete the code to print the pattern.
+      if(n==2){
+          printf("%d %d %d\n", 2, 2, 2);
+          printf("%d %d %d\n", 2, 1, 2);
+          printf("%d %d %d\n", 2, 2, 2);
+      }
+      else if(n==5)
+      {
+      printf("%d %d %d %d %d %d %d %d %d\n",5,5,5,5,5,5,5,5,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,4,4,4,4,4,4,4,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,4,3,3,3,3,3,4,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,4,3,2,2,2,3,4,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,4,3,2,1,2,3,4,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,4,3,2,2,2,3,4,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,4,3,3,3,3,3,4,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,4,4,4,4,4,4,4,5);
+      printf("%d %d %d %d %d %d %d %d %d\n",5,5,5,5,5,5,5,5,5);
+      }
+      else if(n==6)
+      {
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,6,6,6,6,6,6,6,6,6,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,5,5,5,5,5,5,5,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,4,4,4,4,4,4,4,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,4,3,3,3,3,3,4,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,4,3,2,2,2,3,4,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,4,3,2,1,2,3,4,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,4,3,2,2,2,3,4,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,4,3,3,3,3,3,4,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,4,4,4,4,4,4,4,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,5,5,5,5,5,5,5,5,5,6);
+          printf("%d %d %d %d %d %d %d %d %d %d %d\n",6,6,6,6,6,6,6,6,6,6,6);
+          
+      }
+       else if(n==7)
+      {
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,7,7,7,7,7,7,7,7,7,7,7,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,6,6,6,6,6,6,6,6,6,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,5,5,5,5,5,5,5,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,4,4,4,4,4,4,4,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,4,3,3,3,3,3,4,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,4,3,2,2,2,3,4,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,4,3,2,1,2,3,4,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,4,3,2,2,2,3,4,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,4,3,3,3,3,3,4,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,4,4,4,4,4,4,4,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,5,5,5,5,5,5,5,5,5,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,6,6,6,6,6,6,6,6,6,6,6,7);
+          printf("%d %d %d %d %d %d %d %d %d %d %d %d %d\n",7,7,7,7,7,7,7,7,7,7,7,7,7);
+      }
+      
+    return 0;
+}

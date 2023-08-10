52f6f6e076deb770d32864366b63565546fa1a7d
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-10 22:22:38 +0530

    First commit

--- 28.c
+++ 28.c
@@ -4,9 +4,14 @@
 
 int main()
 {
-    int t;
+    int t,n;
+    int i=1;
     int a[n];
     scanf("%d\n", &t);
-    
 
-}
+    while(i<=t){
+        scanf("%d\t", a[n-1]);
+        i=i++;
+    }
+    return 0;
+    }

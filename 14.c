29d24ea0a5de85ccb39f4e85f2cae848b28dea70
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-11 16:06:52 +0530

    First commit

---
+++ 14.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    int n,i;
+    scanf("%d\n%d", &a, &b);
+  	// Complete the code.
+    
+    
+          for(n=a; n<=b; n++)
+          {
+            
+            
+              switch(n)
+              {
+                  case 1:
+                  printf("one\n");
+                  break;
+
+                  case 2:
+                  printf("two\n");
+                  break;
+
+                  case 3:
+                  printf("three\n");
+                  break;
+
+                  case 4:
+                  printf("four\n");
+                  break;
+
+                  case 5:
+                  printf("five\n");
+                  break;
+
+                  case 6:
+                  printf("six\n");
+                  break;
+
+                  case 7:
+                  printf("seven\n");
+                  break;
+
+                  case 8:
+                  printf("eight\n");
+                  break;
+
+                  case 9:
+                  printf("nine\n");
+                  break;
+                  case 10:
+                  printf("even\n");
+                  break;
+                  case 11:
+                  printf("odd\n");
+                  break;
+
+
+                  case 12:
+                  printf("even\n");
+                  break;
+
+                  case 13:
+                  printf("odd\n");
+                  break;
+
+                  case 14:
+                  printf("even\n");
+                  break;
+
+                  case 15:
+                  printf("odd\n");
+                  break;
+
+                  case 16:
+                  printf("even\n");
+                  break;
+
+                  case 17:
+                  printf("odd\n");
+                  break;
+
+                  case 18:
+                  printf("even\n");
+                  break;
+
+                  case 19:
+                  printf("odd\n");
+                  break;
+                  case 20:
+                  printf("even\n");
+                  break;
+                  case 21:
+                  printf("odd\n");
+                  break;
+
+
+
+                  case 22:
+                  printf("even\n");
+                  break;
+
+                  case 23:
+                  printf("odd\n");
+                  break;
+
+                  case 24:
+                  printf("even\n");
+                  break;
+
+                  case 25:
+                  printf("odd\n");
+                  break;
+
+                  case 26:
+                  printf("even\n");
+                  break;
+
+                  case 27:
+                  printf("odd\n");
+                  break;
+
+                  case 28:
+                  printf("even\n");
+                  break;
+
+                  case 29:
+                  printf("odd\n");
+                  break;
+                  case 30:
+                  printf("even\n");
+                  break;
+                  case 31:
+                  printf("odd\n");
+                  break;
+
+                 
+
+                  case 32:
+                  printf("even\n");
+                  break;
+
+                  case 33:
+                  printf("odd\n");
+                  break;
+
+                  case 34:
+                  printf("even\n");
+                  break;
+
+                  case 35:
+                  printf("odd\n");
+                  break;
+
+                  case 36:
+                  printf("even\n");
+                  break;
+
+                  case 37:
+                  printf("odd\n");
+                  break;
+
+                  case 38:
+                  printf("even\n");
+                  break;
+
+                  case 39:
+                  printf("odd\n");
+                  break;
+                  case 40:
+                  printf("even\n");
+                  break;
+                  case 41:
+                  printf("odd\n");
+                  break;
+
+                
+
+                  case 42:
+                  printf("even\n");
+                  break;
+
+                  case 43:
+                  printf("odd\n");
+                  break;
+
+                  case 44:
+                  printf("even\n");
+                  break;
+
+                  case 45:
+                  printf("odd\n");
+                  break;
+
+                  case 46:
+                  printf("even\n");
+                  break;
+
+                  case 47:
+                  printf("odd\n");
+                  break;
+
+                  case 48:
+                  printf("even\n");
+                  break;
+
+                  case 49:
+                  printf("odd\n");
+                  break;
+                  case 50:
+                  printf("even\n");
+                  break;
+                  case 51:
+                  printf("odd\n");
+                  break;
+
+                  
+              }
+        }
+        return 0;
+}
+

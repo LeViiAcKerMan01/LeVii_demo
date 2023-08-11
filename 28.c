cd1ceee9cc2625f1ceb7ea39981fd09a43e91f51
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-11 08:47:54 +0530

    First commit

--- 28.c
+++ 28.c
@@ -10,13 +10,7 @@ int main()
  
     for(i=0; i<n; i++)
     {
-        scanf("%d",&a[i]);
-    }
- 
-    
-    for(i=0; i<n; i++)
-    {
-         
+        scanf("%d\t", &a[i]);
         sum+=a[i];
     }
  

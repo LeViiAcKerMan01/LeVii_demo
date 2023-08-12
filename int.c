06739f66da051725a7273f8fe569d8798205eca7
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-12 23:00:15 +0530

    First commit

--- int.c
+++ int.c
@@ -1,2 +1,17 @@
 #include<stdio.h>
 #include<math.h>
+
+int main()
+{
+    int n;
+    int A,B,t;
+    int i;
+    scanf("%d\n",t);
+    while(i<=t)
+    {
+        scanf("%d %d", &A , &B);
+        printf("%d\n", A+B);
+        i=i++;
+    }
+    return 0;
+}

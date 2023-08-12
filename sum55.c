68615b4ffa387affca9090118933f1203918fc50
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-12 11:02:20 +0530

    First commit

--- sum55.c
+++ sum55.c
@@ -4,31 +4,78 @@
 
 int main()
 {
-    int n,a,A,b,B,c,C,d,D,e,E,F;
+    int n,a,A,b,B,c,C,d,D,F;
+    scanf("%d", &n);
 
-    a=n/10000;
-    A=n%10000;
+    if(n>10000 && A>=1000)
+    {
+        a=n/10000;
+        A=n%10000;
+        printf("%d\n", a);
+        printf("%d\n", A);
 
-    b=A/1000;
-    B=A%1000;
+        b=A/1000;
+        B=A%1000;
+        printf("%d\n", b);
+        printf("%d\n", B);
+        
+        if(B>=100 && C>=10)
+        {
+        c=B/100;
+        C=B%100;
+        printf("%d\n", c);
+        printf("%d\n", C);
 
-    c=B/100;
-    C=B%100;
+        d=C/10;
+        D=C%10;
+        printf("%d\n", d);
+        printf("%d\n", D);
+        
+        if(D>=1)
+        {
+            F=a+b+c+d+D;
+            printf("%d\n", F);
+        }
 
-    d=C/10;
-    D=C%10;
-
-    e=D/1;
-
-    F=a+b+c+d+e;
+        }
 
 
-    if(A>=1000 && B>=100){
-
-
-        printf("%d\n", F);    
     }
-   
+    else if(A<1000 && B<100)
+    {
+        b=A/100;
+        B=A%100;
+        printf("%d\n", b);
+        printf("%d\n", B);
 
+    
+        
+        c=B/10;
+        C=B%10;
+        printf("%d\n", c);
+        printf("%d\n", C);
+
+        if(B<=10)
+        {
+            c=B/1;
+            C=B%1;
+            printf("%d\n", b);
+            printf("%d\n", B);
+            
+            F=A+B+c;
+            printf("%d\n", F);
+
+        }
+    else if(A<1000 && B<10)
+    {
+        
+        
+
+    }
+
+        
+    }
+
+    
     return 0;
 }

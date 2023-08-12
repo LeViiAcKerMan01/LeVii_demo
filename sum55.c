dff6bd8a8010b0a0bd717db7f9361e62164a2460
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-12 10:59:39 +0530

    First commit

--- sum55.c
+++ sum55.c
@@ -5,7 +5,7 @@
 int main()
 {
     int n,a,A,b,B,c,C,d,D,e,E,F;
-/*
+
     a=n/10000;
     A=n%10000;
 
@@ -21,24 +21,10 @@ int main()
     e=D/1;
 
     F=a+b+c+d+e;
-*/
+
 
     if(A>=1000 && B>=100){
-    a=n/10000;
-    A=n%10000;
 
-    b=A/1000;
-    B=A%1000;
-
-    c=B/100;
-    C=B%100;
-
-    d=C/10;
-    D=C%10;
-
-    e=D/1;
-
-    F=a+b+c+d+e;
 
         printf("%d\n", F);    
     }

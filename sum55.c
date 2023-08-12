42fbfde3e5f93e1c88720d9723705f5385c43a12
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-12 12:42:19 +0530

    First commit

--- sum55.c
+++ sum55.c
@@ -1,85 +1,21 @@
 #include<stdio.h>
-#include<string.h>
 #include<math.h>
 
 int main()
 {
-    int n,a,A,b,B,c,C,d,D,F;
-    scanf("%d", &n);
-
-    if(n==10000){
-        printf("%d\n", 1);
-    }
-
-    else if(n>10000 && a>=1)
-    {
-        a=n/10000;
-        A=n%10000;
-        printf("%d\n", a);
-        printf("%d\n", A);
-
-        b=A/1000;
-        B=A%1000;
-        printf("%d\n", b);
-        printf("%d\n", B);
-        
-        if(B>=100 && c>=1)
-        {
-        c=B/100;
-        C=B%100;
-        printf("%d\n", c);
-        printf("%d\n", C);
-
-        d=C/10;
-        D=C%10;
-        printf("%d\n", d);
-        printf("%d\n", D);
-        
-        if(D>=1)
-        {
-            F=a+b+c+d+D;
-            printf("%d\n", F);
-        }
-
-        }
-
-
-    }
-    else if(A<1000 && B<100)
-    {
-        b=A/100;
-        B=A%100;
-        printf("%d\n", b);
-        printf("%d\n", B);
-
+    int n;
+    int a,b;
+   
     
+    scanf("%d", &n);
+    
+    
+    if(a>=1 && b>=1){
+        a=n/10000;
+        b=a/1000;
         
-        c=B/10;
-        C=B%10;
-        printf("%d\n", c);
-        printf("%d\n", C);
-
-        if(B>=10)
-        {
-            c=B/10;
-            C=B%10;
-            printf("%d\n", b);
-            printf("%d\n", B);
-            
-            F=a+b+c;
-            printf("%d\n", F);
-
-        }
-    else if(A<1000 && B<10)
-    {
-        
-        
-
+        printf("%d\n", a+b);
     }
-
-        
-    }
-
     
     return 0;
 }

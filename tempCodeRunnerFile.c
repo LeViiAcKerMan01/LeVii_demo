435fa8c459b550685af4f4e29c808236c04667f1
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-12 11:10:29 +0530

    First commit

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,19 +1,2 @@
-#include <stdio.h>
-#include <conio.h>
- 
- 
-int main()
-{
-    int a[1000],i,n,sum=0;
-   
-    scanf("%d",&n);
- 
-    for(i=0; i<n; i++)
-    {
-        scanf("%d\t", &a[i]);
-        sum+=a[i];
-    }
-    printf("%d\n",sum);
- 
-    return 0;
-}
+
+#include<math.h>

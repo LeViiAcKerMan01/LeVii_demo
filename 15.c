7509984cf784ffc65e144cb9ac8da5b84f12eeb6
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-13 19:33:15 +0530

    First commit

---
+++ 15.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[30]="Codechef ";
+    char str2[]="Academy";
+    strcat(str, str2);
+    printf("%s\n", str);
+    return 0;
+
+    
+}

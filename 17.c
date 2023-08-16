3092881b32f888ade879cec5c257971c2dfa2ebd
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-16 11:36:42 +0530

    First commit

--- 17.c
+++ 17.c
@@ -9,11 +9,11 @@ int main()
     int age;
     
     printf("Enter your Name:");
-    scanf("%[^\n]s", N);
+    scanf("%s\n", N);
     printf("enter your age:");
     scanf("%d\n", &age);
     printf("Enter your academy:");
-    scanf("%[^\n]s", str);
+    scanf("%s\n", str);
     printf("Your name is %s\n", N);
     printf("You are %d years old\n", age);
     printf("Your academy goes by the name", str);

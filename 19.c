63c5c36f40a5362afbd668532e9e7ee4bed08c07
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-08-26 20:59:04 +0530

    First commit

--- 19.c
+++ 19.c
@@ -3,5 +3,28 @@ int main()
 {
     int a;
     int b;
-     
+    char op;
+    printf("Enter first number:");
+    scanf("%d", &a);
+    printf("Enter Second number:");
+    scanf("%d", &b);
+    printf("Enter operator:");
+    scanf("%s", &op);
+    switch(op){
+        case '+':
+        printf("%d", a+b);
+        break;
+        case '-':
+        printf("%d", a-b);
+        break;
+        case '/':
+        printf("%d", a/b);
+        break;
+        case '*':
+        printf("%d", a*b);
+        break;
+    }
+return 0;
+
+
 }
